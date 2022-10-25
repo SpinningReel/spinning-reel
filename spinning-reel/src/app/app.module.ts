c7c8a30670190b50e9fc896e49f09c6f2757d5ae
kelby <kelbytansley.kt@gmail.com>
2022-10-25 21:53:23 +0200

    Include webflow files

--- spinning-reel/src/app/app.module.ts
+++ spinning-reel/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { DescriptionWrapperComponent } from './description-wrapper/description-wrapper.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { AboutComponent } from './about/about.component';
+import { AttractionsComponent } from './attractions/attractions.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FeatureComponent } from './feature/feature.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    DescriptionWrapperComponent,
+    AccomodationComponent,
+    AboutComponent,
+    AttractionsComponent,
+    TestimonialsComponent,
+    FeatureComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ spinning-reel/src/app/description-wrapper/description-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-description-wrapper',
+  templateUrl: './description-wrapper.component.html',
+  styleUrls: ['./description-wrapper.component.scss']
+})
+export class DescriptionWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
